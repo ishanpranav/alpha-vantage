0a67642eca578e4a82ff652c393276f2cc6b0923
Ishan Pranav <ishan.pranav@outlook.com>
2024-04-29 14:44:47 -0400

    Add some test code

--- src/AlphaVantageCore.App/Program.cs
+++ src/AlphaVantageCore.App/Program.cs
@@ -2,10 +2,11 @@
 // Copyright (c) 2023 Ishan Pranav. All rights reserved.
 // Licensed under the MIT License.
 
+using CsvHelper;
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace AlphaVantageCore.App;
@@ -23,14 +24,40 @@ internal static class Program
 
         using AlphaVantageClient client = new AlphaVantageClient(apiKey);
 
-        var response = await client.GetTimeSeriesAsync(
-            new AlphaVantageIntradayTimeSeriesRequest()
-            {
-                OutputSize = AlphaVantageOutputSize.Full,
-                Symbol = "QQQ",
-                Interval = AlphaVantageInterval.Minute
-            });
+        DateTime end = DateTime.Now;
+        SortedDictionary<DateTime, AlphaVantageTimeSeriesResponse> items = new SortedDictionary<DateTime, AlphaVantageTimeSeriesResponse>();
 
-        File.WriteAllText("qqq-time-series.json", response[0].Content);
+        for (DateTime month = new DateTime(2000, 1, 1);
+            month < end;
+            month = month.AddMonths(1))
+        {
+            await Console.Out.WriteLineAsync($"GET {month:MMM yyyy}\tn = {items.Count:n0}");
+
+            var response = await client.GetTimeSeriesAsync(
+                new AlphaVantageIntradayTimeSeriesRequest()
+                {
+                    OutputSize = AlphaVantageOutputSize.Full,
+                    Symbol = "QQQ",
+                    Interval = AlphaVantageInterval.Minute,
+                    Month = month
+                });
+
+            foreach (var item in response)
+            {
+                items[item.Timestamp] = item;
+            }
+        }
+
+        using TextWriter textWriter = File.CreateText("qqq-time-series.csv");
+        using CsvWriter writer = new CsvWriter(textWriter, CultureInfo.CurrentCulture);
+
+        writer.WriteHeader<AlphaVantageTimeSeriesResponse>();
+        writer.NextRecord();
+
+        foreach (var value in items.Values)
+        {
+            writer.WriteRecord(value);
+            writer.NextRecord();
+        }
     }
 }
